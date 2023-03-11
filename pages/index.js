d0c0b11f53a718d19923b32190d119841bb1499d
marcushugo <phantomboss300@yahoo.com>
2023-03-11 13:58:45 -0600

    added Head components for title attributes

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,18 @@
+import Head from "next/head"
+
 export default function Home(props) {
   return (
-    <main>
-      <div>
-        <h1>Hi, I'm Marcus Hugo.</h1>
-        <h2>Frontend Web Developer.</h2>
-        <p>I'm a passionate self-taught frontend web developer, with over 2 years experience creating user friendly, accessible, and responsive web experiences.</p>
-      </div>
-    </main>
+    <>
+      <Head>
+        <title>Marcus Hugo's Blog</title>
+      </Head>
+      <main>
+        <div>
+          <h1>Hi, I'm Marcus Hugo.</h1>
+          <h2>Frontend Web Developer.</h2>
+          <p>I'm a passionate self-taught frontend web developer, with over 2 years experience creating user friendly, accessible, and responsive web experiences.</p>
+        </div>
+      </main>
+    </>
   )
 }
--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,13 +1,19 @@
 import Link from "next/link"
+import Head from "next/head"
 
 export default function FirstPost() {
   return (
-    <main>
-      <article>
-        <h1>Learing how to build a blog with Next.js!</h1>
-        <p>I'm following the best tutorial/documentation on Next/React! I've also watched Brad Schiff's YouTube video 'Next.js Tutorial For Beginners', which is also great!</p>
-        <p>Next has feature called client-side navigation, which is faster than regular browser navigation. By using javascript and loading the full page when navigating. When a Link component is scrolled into view Next will load that page content.</p>
-      </article>
-    </main>
+    <>
+      <Head>
+        <title>Learning how to build a blog with Next.js</title>
+      </Head>
+      <main>
+        <article>
+          <h1>Learing how to build a blog with Next.js!</h1>
+          <p>I'm following the best tutorial/documentation on Next/React! I've also watched Brad Schiff's YouTube video 'Next.js Tutorial For Beginners', which is also great!</p>
+          <p>Next has feature called client-side navigation, which is faster than regular browser navigation. By using javascript and loading the full page when navigating. When a Link component is scrolled into view Next will load that page content.</p>
+        </article>
+      </main>
+    </>
   )
 }
